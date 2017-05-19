da93561855017dfae4c274efc64130b49b9569ba
BluePawDev <jaruby@me.com>
2017-05-19 10:00:21 -0500

    Add comments re: .forEach();

--- vendors/script.js
+++ vendors/script.js
@@ -1,1 +1,7 @@
 console.log('js/jq sourced!');
+
+
+// .forEach() loops through an array and retruns the index of...
+// boxContents.forEach(function([element/currentValue], [index], [array]));
+// function logTheBoxContents(boxArrayElement, index);
+// console.log('The box at index', index, 'is', boxArrayElement);
